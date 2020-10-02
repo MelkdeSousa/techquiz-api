de9e298f5d80c44fcfd3bcb2de65d06f825faaa2
MelkdeSousa <melk.sousap@gmail.com>
2020-10-02 03:36:41 -0300

    🎲added typeorm and configured

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+module.exports = {
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/models/*.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+    entitiesDir: 'src/models'
+  }
+}
